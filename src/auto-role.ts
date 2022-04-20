c217d09ed8eacee3759b78287c0bc4c440d4c7e7
Ceci <admin@cecilias.me>
2022-04-20 12:59:03 -0500

    chore: refactor

---
+++ src/auto-role.ts
@@ -0,0 +1,98 @@
+import { Client, GuildMember, PartialGuildMember } from 'discord.js';
+import { ROLES, STAFF_ROLES } from './roles';
+import { ROLE_CHANGED, wasAnyRoleChanged } from './utils/roles';
+
+export class AutoRolesDiff {
+    private _added: ROLES[] = [];
+    private _removed: ROLES[] = [];
+
+    add(...roles: ROLES[]) {
+        for (const role of roles) {
+            const removedIndex = this._removed.indexOf(role);
+
+            if (removedIndex !== -1) {
+                this._removed.splice(removedIndex, 1);
+            }
+        }
+    }
+    
+    remove(...roles: ROLES[]) {
+        for (const role of roles) {
+            const addedIndex = this._added.indexOf(role);
+
+            if (addedIndex !== -1) {
+                this._removed.splice(addedIndex, 1);
+            }
+        }
+    }
+
+    get added(): ROLES[] {
+        return this._added;
+    }
+
+    get removed(): ROLES[] {
+        return this._removed;
+    }
+}
+
+export async function submitDiff(diff: AutoRolesDiff, member: GuildMember | PartialGuildMember) {
+    const promises = [];
+
+    if (diff.added.length) {
+        promises.push(member.roles.add(diff.added));
+    }
+
+    if (diff.removed.length) {
+        promises.push(member.roles.remove(diff.removed));
+    }
+
+    await Promise.all(promises);
+}
+
+export function autoVerify(diff: AutoRolesDiff, oldMember: GuildMember | PartialGuildMember, newMember: GuildMember | PartialGuildMember) {
+    if (oldMember.pending && !newMember.pending) {
+        diff.add(
+            ROLES.GUESTS,
+            ROLES.PRONOUNS_HEADER,
+            ROLES.PRONOUNS_FOOTER,
+        );
+    }
+}
+
+export function autoGuestStaff(diff: AutoRolesDiff, oldMember: GuildMember | PartialGuildMember, newMember: GuildMember | PartialGuildMember) {
+    const STAFF_CHANGED = wasAnyRoleChanged(oldMember, newMember, STAFF_ROLES);
+
+    if (STAFF_CHANGED === ROLE_CHANGED.ADDED) {
+        diff.remove(ROLES.GUESTS);
+        diff.add(ROLES.STAFF);
+    } else if (STAFF_CHANGED === ROLE_CHANGED.REMOVED) {
+        diff.remove(ROLES.STAFF);
+        diff.add(ROLES.GUESTS);
+    }
+}
+
+export async function onGuildMemberUpdate(oldMember: GuildMember | PartialGuildMember, newMember: GuildMember | PartialGuildMember) {
+    const diff = new AutoRolesDiff();
+
+    // TODO: Remove once Sapphire supports Bot Join Roles
+    autoVerify(diff, oldMember, newMember);
+    autoGuestStaff(diff, oldMember, newMember);
+
+    await submitDiff(diff, newMember);
+}
+
+export async function onGuildMemberAdd(member: GuildMember | PartialGuildMember) {
+    const diff = new AutoRolesDiff();
+
+    // TODO: Remove once Sapphire supports Bot Join Roles
+    if (member.user.bot) {
+        diff.add(ROLES.BOTS);
+    }
+
+    await submitDiff(diff, member);
+}
+
+export function autoRoleSetup(client: Client) {
+    client.on('guildMemberAdd', onGuildMemberAdd);
+    client.on('guildMemberUpdate', onGuildMemberUpdate);
+}
--- src/bot.ts
+++ src/bot.ts
@@ -1,6 +1,6 @@
 import { Client } from 'discord.js';
-import { ROLES, STAFF_ROLES } from './roles';
-import { ROLE_CHANGED, wasAnyRoleChanged } from './utils/roles';
+import { autoRoleSetup } from './auto-role';
+import { modMailSetup } from './mod-mail';
 
 const client = new Client({
     partials: [
@@ -16,38 +16,11 @@ client.on('ready', async () => {
     if (!client.user || !client.application) return;
 
     console.log(`${client.user.username} is online.`);
+
+    await Promise.all([
+        autoRoleSetup(client),
+    ]);
 });
 
-// TODO: Remove once Sapphire supports Bot Join Roles
-client.on('guildMemberAdd', async (member) => {
-    if (member.user.bot) {
-        await member.roles.add(ROLES.BOTS);
-    }
-});
-
-client.on('guildMemberUpdate', async (oldMember, newMember) => {
-    // TODO: Remove once Sapphire supports Bot Join Roles
-    if (oldMember.pending && !newMember.pending) {
-        await newMember.roles.add([
-            ROLES.GUESTS,
-            ROLES.PRONOUNS_HEADER,
-            ROLES.PRONOUNS_FOOTER,
-        ]);
-    }
-
-    const STAFF_CHANGED = wasAnyRoleChanged(oldMember, newMember, STAFF_ROLES);
-
-    if (STAFF_CHANGED === ROLE_CHANGED.ADDED) {
-        await Promise.all([
-            newMember.roles.remove(ROLES.GUESTS),
-            newMember.roles.add(ROLES.STAFF),
-        ]);
-    } else if (STAFF_CHANGED === ROLE_CHANGED.REMOVED) {
-        await Promise.all([
-            newMember.roles.remove(ROLES.STAFF),
-            newMember.roles.add(ROLES.GUESTS),
-        ]);
-    }
-});
 
 client.login(process.env.DISCORD_TOKEN);
