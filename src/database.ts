9138e6db0c568f8f3571b6a9e1f034fc2dd084ca
Ceci <admin@cecilias.me>
2022-04-20 13:54:07 -0500

    chore: more logging

--- src/database.ts
+++ src/database.ts
@@ -9,7 +9,7 @@ export async function redis(): Promise<Redis> {
     }
     
     if (process.env.REDISTOGO_URL) {
-        console.log('Connecting to production database');
+        console.log('Connecting to production database...');
         const info = new URL(process.env.REDISTOGO_URL)
         _client = new Redis({
             host: info.hostname,
@@ -17,16 +17,27 @@ export async function redis(): Promise<Redis> {
             password: info.password
         });
     } else {
+        console.log('Starting in-memory database...');
         const { RedisMemoryServer } = await import('redis-memory-server');
         const redisServer = new RedisMemoryServer();
 
         await redisServer.start();
+        console.log('Successfully started database!');
 
+        console.log('Connecting to in-memory database...');
         _client = new Redis({
             port: redisServer.instanceInfoSync?.port,
             host: redisServer.instanceInfoSync?.ip,
         });
     }
+
+    _client.on('connect', () => {
+        console.log('Connection established!');
+    })
+
+    _client.on('reconnecting', () => {
+        console.log('Reconnecting to database...');
+    })
 
     _client.on('error', console.error);
 
