d3ec4a8d8c3df3bce354b012e026e9bae9522536
Ceci <admin@cecilias.me>
2022-04-20 20:13:01 -0500

    chore: utilize builders

--- src/funtionality/welcome.ts
+++ src/funtionality/welcome.ts
@@ -1,4 +1,5 @@
 import { Client, GuildMember, PartialGuildMember } from 'discord.js';
+import { userMention, roleMention } from '@discordjs/builders';
 import endent from 'endent';
 import { CONFIG } from '../config';
 
@@ -12,13 +13,13 @@ export async function onGuildMemberAdd(this: Client, member: GuildMember | Parti
                     url: 'https://imgur.com/1SjRXpE.png'
                 },
                 description: endent`
-                    **Welcome to Rainbow Café, <@${member.user.id}>** <:wave:966058854519537775>
+                    **Welcome to Rainbow Café, ${userMention(member.user.id)}** <:wave:966058854519537775>
                     　
                     Please read and accept the rules below! Afterward, you'll be granted the \'Guest\' role. This role is for players that aren't part of the FC!
                     　
                     If you are part of the FC head on over to <#966194062329774141> after accepting the rules and type \`!verifyme\` and it'll setup your rank!
                     　
-                    - <@&966056005886677052>, <@&965769109461676053>, and <@&965675644526690346>
+                    - ${roleMention('966056005886677052')}, ${roleMention('965769109461676053')}, and ${roleMention('965675644526690346')}
                 `,
                 color: '#F7A8B8',
             }],
