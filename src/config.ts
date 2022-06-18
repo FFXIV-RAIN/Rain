2de0b84976b0b10c31544342a80aac25104998ba
Ceci <admin@cecilias.me>
2022-06-18 00:26:10 -0500

    chore: added more logging

--- src/config.ts
+++ src/config.ts
@@ -5,13 +5,13 @@ export function isFeatureFlagEnabled(name: string) {
     return Boolean(process.env[`FF_${name}`] || 'true');
 }
 
-export function get(defaultValue: string, ...names: string[]): string {
+export function get<T>(defaultValue: T, ...names: string[]): T {
     for (const name of names) {
         const value = process.env[name];
 
         if (!value) continue;
 
-        return value;
+        return value as unknown as T;
     }
 
     return defaultValue;
@@ -30,7 +30,7 @@ export interface FeatureFlags {
     WELCOME: boolean;
 }
 
-const ENVIRONMENT = get(Environment.LOCAL, 'ENVIRONMENT') as Environment;
+const ENVIRONMENT = get<Environment>(Environment.LOCAL, 'ENVIRONMENT');
 
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 
@@ -38,9 +38,8 @@ if (!DISCORD_TOKEN) throw new Error(`Token cannot be null or undefined`);
 
 export const CONFIG: Config = {
     DISCORD_TOKEN,
-    
     DATABASE_URL: get('sqlite::memory:', 'DATABASE_URL'),
     ENVIRONMENT,
-    LOG_LEVEL: LOG_LEVEL.INFO,
+    LOG_LEVEL: get<LOG_LEVEL>(LOG_LEVEL.INFO, 'LOG_LEVEL'),
     IS_LIVE: ENVIRONMENT === Environment.LIVE,
 }
--- src/modules/welcome.module.ts
+++ src/modules/welcome.module.ts
@@ -1,15 +1,24 @@
 import { Client, GuildMember, PartialGuildMember } from 'discord.js';
+import { logger } from '../utils/logger';
 import { Configs } from '../services/configs.service';
 import { parseMessage } from '../utils/message';
 
 export async function onGuildMemberAdd(this: Client, member: GuildMember | PartialGuildMember) {
     const welcome = await Configs.welcome(member.guild.id);
 
+    logger.trace(`WelcomeConfig: ${JSON.stringify(welcome)} , Bot: ${member.user.bot}`);
+
     if (!welcome || welcome.disabled || !welcome.channelId || !welcome.message || (welcome.ignoreBots && member.user.bot)) return;
+
+    logger.trace(`Getting channel...`);
 
     const channel = await this.channels.cache.get(welcome.channelId);
 
+    logger.trace(`Channel retrieved! (exists: ${Boolean(channel)})`);
+
     if (!channel || !channel.isText()) return;
+
+    logger.trace(`Sending welcome message...`);
     
     channel.send({
         embeds: [{
