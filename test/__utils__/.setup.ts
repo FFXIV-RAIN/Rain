a7a910d4bdf55ae207e2e7486982415434a3b976
Ceci <admin@cecilias.me>
2022-09-02 01:03:07 -0500

    ci: test failures

--- test/__utils__/.setup.ts
+++ test/__utils__/.setup.ts
@@ -1,6 +1,6 @@
 jest.mock('../../src/db/models/Guild/Guild');
 jest.mock('../../src/db/models/Guild/GuildConfig');
-jest.mock('../../src/db/models/modules/AutoRoleConfig');
+jest.mock('../../src/db/models/modules/AutoRole/AutoRoleConfig');
 jest.mock('../../src/db/models/modules/WelcomeConfig');
 
 process.env.DISCORD_TOKEN = '<test-token>';
